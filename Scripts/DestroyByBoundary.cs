1f9134412c7ac15eb560f0fd2b7a665f53a6eed9
Patrick Lai <Patrick Lai>
2017-03-03 14:38:20 -0500

    add files to repo

---
+++ Scripts/DestroyByBoundary.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByBoundary : MonoBehaviour {
+	//private void OnTriggerEnter(Collider other)
+	//{
+	//	Object.Destroy(other.gameObject.transform.parent.gameObject);
+	//}
+
+	void OnTriggerExit(Collider other)
+	{
+		Destroy(other.gameObject);
+	}
+}
---
+++ Scripts/MovieScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovieScript : MonoBehaviour {
+
+	public MovieTexture movTexture;
+
+	void Start () {
+		GetComponent<Renderer>().material.mainTexture = movTexture;
+		movTexture.loop = true;
+		movTexture.Play();
+		
+		
+	}
+
+	private void Update()
+	{
+		if (!movTexture.isPlaying)
+			movTexture.Play();
+	}
+}
